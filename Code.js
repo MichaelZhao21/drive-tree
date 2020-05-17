47da03b15552d38db9b52115cc74ea200a12f0b9
MichaelZhao21 <michaelzhao314@gmail.com>
2020-05-17 19:07:45 -0400

    Added basic framework

--- Code.js
+++ Code.js
@@ -1,3 +1,8 @@
-function myFunction() {
-  
+function doGet() {
+	file = HtmlService.createTemplateFromFile('Index').evaluate();
+	return file;
+}
+
+function include(filename) {
+	return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
