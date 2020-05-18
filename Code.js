74fab2cdb23b29f3c127511fb6830811e10ef476
MichaelZhao21 <michaelzhao314@gmail.com>
2020-05-17 21:05:43 -0400

    Added dfs through files into Node object

--- Code.js
+++ Code.js
@@ -6,3 +6,36 @@ function doGet() {
 function include(filename) {
 	return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
+
+function getTree() {
+	var root = DriveApp.getRootFolder().getFoldersByName("Robotics").next();
+	var rootNode = new FolderNode(root);
+	var toProcess = [rootNode];
+	while (toProcess.length) {
+		var next = toProcess.shift();
+		genFiles(next);
+		toProcess = toProcess.concat(next.subfolders);
+		Logger.log(next.folder);
+		Logger.log(toProcess);
+	}
+	Logger.log(rootNode.subfolders[0].subfolders);
+}
+
+function genFiles(node) {
+	var folderIt = node.folder.getFolders();
+	var fileIt = node.folder.getFiles();
+	while (folderIt.hasNext()) {
+		node.subfolders.push(new FolderNode(folderIt.next()));				
+	}
+	while (fileIt.hasNext()) {
+		node.files.push(fileIt.next());
+	}
+}
+
+class FolderNode {
+	constructor(folder) {
+		this.folder = folder;
+		this.subfolders = [];
+		this.files = [];
+	}
+}
